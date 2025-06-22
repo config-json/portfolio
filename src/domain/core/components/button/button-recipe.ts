065bc9cae43e8f3c2e1eaa8bd6c07a329b775b53
config-json <configjson@proton.me>
2025-06-22 20:21:33 +0300

    Bump and refactor

---
+++ src/domain/core/components/button/button-recipe.ts
@@ -0,0 +1,19 @@
+import { cn } from "../../utils/cn";
+import { defineRecipe, RecipeProps } from "../../utils/define-recipe";
+
+export const buttonRecipe = defineRecipe({
+    base:
+      cn(
+        "rounded-sm border-2 py-1.5 px-3",
+        "transition duration-300 hover:bg-white hover:text-background hover:border-white",
+      ),
+    variants: {
+      background: {
+        white: "bg-white text-background border-white hover:opacity-70",
+        accent: "bg-accent text-white border-accent",
+        stroke: "border-white text-white",
+      },
+    }
+})
+
+export type ButtonVariantProps = RecipeProps<typeof buttonRecipe>
---
+++ src/domain/core/components/button/button.tsx
@@ -0,0 +1,18 @@
+import { FC, PropsWithChildren } from "react";
+import { buttonRecipe } from "./button-recipe";
+
+type ButtonProps = PropsWithChildren & {
+  onClick?: () => void;
+  background: "white" | "accent" | "stroke";
+}
+
+export const Button: FC<ButtonProps> = ({ children, onClick, background }) => {
+  return (
+    <button
+      onClick={onClick}
+      className={buttonRecipe({ background })}
+    >
+      {children}
+    </button>
+  );
+}
